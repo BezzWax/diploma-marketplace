14417e490966060ff4a4679145bd97e40fcd1d49
Eugene <87862942+BezzWax@users.noreply.github.com>
2024-04-06 17:38:08 +0300

    Delete online-marketplace directory

--- online-marketplace/server/routes/brandRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const brandController = require('../controllers/brandController')
-const checkRole = require('../middleware/checkRoleMiddleware');
-
-router.post('/',checkRole('ADMIN'), brandController.create);
-router.get('/', brandController.getAll);
-
-module.exports = router;
